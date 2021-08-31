ee5133f991c9c440ac503bb6456c09aeb9caf97e
Apdo3939 <apdo3939@hotmail.com>
2021-08-31 16:19:57 -0300

    Add section services and contact

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,9 @@ $(function () {
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
-    })
+    });
+
+    $('.navbar .menu li a').click(function () {
+        $('html').css('scrollBehavior', 'smooth');
+    });
 })
